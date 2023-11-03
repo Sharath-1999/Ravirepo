16a0a73a5f709220c49cf91c38ffdc638dfe178d
Sharath-1999 <sharathchandraracha@gmail.com>
2023-11-03 15:46:11 +0530

    added two more classes

---
+++ ShahrukhProject/src/main/java/threeclass.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class threeclass {
+
+	@Test
+	public void three()
+	{
+		System.out.println("this is three");
+	}
+}
---
+++ ShahrukhProject/src/main/java/twoclass.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class twoclass {
+	
+	@Test
+	public void two()
+	{
+		System.out.println("This is two");
+	}
+
+}
