8245a22dc16d74745c89c88c5605d6323485e2b2
Sharath-1999 <sharathchandraracha@gmail.com>
2023-11-03 15:52:03 +0530

    edited the two class

--- ShahrukhProject/src/main/java/twoclass.java
+++ ShahrukhProject/src/main/java/twoclass.java
@@ -6,6 +6,8 @@ public class twoclass {
 	public void two()
 	{
 		System.out.println("This is two");
+		System.out.println("edited something");
+		System.out.println("edited twice something");
 	}
 
 }
